63ba82afa624dd604358d153494046765f0f0f2f
phpBBeesti <info@phpbbeesti.net>
2017-07-23 17:10:28 +0300

    Updated
    July 23, 2017

--- captcha_recaptcha.php
+++ captcha_recaptcha.php
@@ -1,13 +1,20 @@
 <?php
 /**
 *
-* This file is part of the phpBB Forum Software package.
+* This file is part of Estonian phpBB translation.
+* Copyright (c) 2008 - 2017 Estonian translation group.
 *
-* @copyright (c) phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
 *
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+*
+* @package      Estonian
+* @author       2017-07-23 phpBBeesti.net <et.translations@phpbbeesti.net>
+* @copyright    phpBBeesti
+* @license      @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
@@ -37,16 +44,28 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'RECAPTCHA_LANG'				=> 'en',
-	'RECAPTCHA_NOT_AVAILABLE'		=> 'In order to use reCaptcha, you must create an account on <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
-	'CAPTCHA_RECAPTCHA'				=> 'reCaptcha',
-	'RECAPTCHA_INCORRECT'			=> 'The solution you provided was incorrect',
-	'RECAPTCHA_NOSCRIPT'			=> 'Please enable JavaScript in your browser to load the challenge.',
+	'CAPTCHA_QA'				=> 'K&amp;V CAPTCHA',
+	'CONFIRM_QUESTION_EXPLAIN'	=> 'See küsimus võimaldab ennetada automaatseid sisestusi.',
+	'CONFIRM_QUESTION_WRONG'	=> 'Sisestasid ebakorrektse kinnitusküsimuse.',
+	'CONFIRM_QUESTION_MISSING'	=> 'Kontrollküsimust CAPTCHA jaoks ei õnnestunud kuvada. Palun võtke ühendust foorumi administraatoriga.',
 
-	'RECAPTCHA_PUBLIC'				=> 'Public reCaptcha key',
-	'RECAPTCHA_PUBLIC_EXPLAIN'		=> 'Your public reCaptcha key. Keys can be obtained on <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
-	'RECAPTCHA_PRIVATE'				=> 'Private reCaptcha key',
-	'RECAPTCHA_PRIVATE_EXPLAIN'		=> 'Your private reCaptcha key. Keys can be obtained on <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'QUESTION_ANSWERS'			=> 'Vastused',
+	'ANSWERS_EXPLAIN'			=> 'Palun sisesta lubatavad vastused küsimusele, üks iga rea kohta.',
+	'CONFIRM_QUESTION'			=> 'Küsimus',
 
-	'RECAPTCHA_EXPLAIN'				=> 'In an effort to prevent automatic submissions, we require that you complete the following challenge.',
+	'ANSWER'					=> 'Vastus',
+	'EDIT_QUESTION'				=> 'Muuda küsimust',
+	'QUESTIONS'					=> 'Küsimused',
+	'QUESTIONS_EXPLAIN'			=> 'Registreerimise ajal küsitakse kasutajalt ühte neist küsimustest. Et kasutada seda lisa, pead sisestama vähemalt ühe küsimuse vaikimisi keeles. Need küsimused peaksid olema lihtsad inimestele vastamiseks, kuid võimatud robotidel neile veebiotsingute teel vastata. Parima tulemuse toob paljude ja regulaarselt muudetevate küsimuste kasutamine. Luba range seade, kui õige vastus sõltub tõusutundlikkusest ja kirjavahemärkidest.',
+	'QUESTION_DELETED'			=> 'Küsimus kustutatud',
+	'QUESTION_LANG'				=> 'Keel',
+	'QUESTION_LANG_EXPLAIN'		=> 'Keel, milles see küsimused ja tema vastused on kirjutatud.',
+	'QUESTION_STRICT'			=> 'Range kontroll',
+	'QUESTION_STRICT_EXPLAIN'	=> 'Kui lubatud, eristatakse suurtähti ja tühikuid.',
+
+	'QUESTION_TEXT'				=> 'Küsimus',
+	'QUESTION_TEXT_EXPLAIN'		=> 'Küsimus, mis esitatakse registreerumisel.',
+
+	'QA_ERROR_MSG'				=> 'Palun täida kõik väljad ja sisesta vähemalt üks vastus.',
+	'QA_LAST_QUESTION'			=> 'Sa ei saa kustutada kõiki küsimusi, kuni moodul on aktiivne.'
 ));
